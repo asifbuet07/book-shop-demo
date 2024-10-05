17268abfeffaba9574c3cc6e99ebd920f01191fa
DESKTOP-54ATNKA\ASIF <asifbuet07@gmail.com>
2024-10-05 23:41:38 +0600

    Added Footer

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { Navbar } from "../components/shared/Navbar";
 import Banner from "../components/banner";
 import BookList from "../components/book/booklist";
+import Footer from "../components/shared/Footer";
 
 export const Homepage = () => {
   return (
-    <div>
+    <div className="w-full">
       <Navbar />
       <Banner />
       <BookList />
+      <Footer />
     </div>
   );
 };
